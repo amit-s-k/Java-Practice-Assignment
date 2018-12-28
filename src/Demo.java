bfaa7240e55cd948149f831bdcf82927442319c3
amit-s-k <amitsk2014@gmail.com>
2018-12-28 22:55:33 +0530

    Deleted unnecessary files

--- src/Demo.java
+++
@@ -1,5 +0,0 @@
-public class Demo {
-    public void log(){
-        System.out.println("hey");
-    }
-}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String []args){
-        Demo demo=new Demo();
-        demo.log();
-
-    }
-}
